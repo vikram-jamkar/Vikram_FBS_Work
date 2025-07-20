5e56d6bb72f27ce379571eca06bc91e1d0b36dcc
Vikram jamkar <vikram.jamkar2275@gmail.com>
2025-07-21 00:00:19 +0530

    Add files via upload

---
+++ Assiment/Assignment1/Q5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//Q5. Write a program to check whether a person is eligible to vote (age ≥ 18).
+void main (){
+    int age;
+    
+    printf("please enter age ");
+    scanf("%d",&age);
+
+ 
+ 
+   
+    if (age >= 18){
+        printf("you are  vote eligell");
+    }else if (age <= 18){
+        printf("not vote elegibl");
+    } age++;
+ 
+   }
