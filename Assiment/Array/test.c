0e6c3650de6586e625f96ba9e165ca8661e70bbd
Vikram jamkar <vikram.jamkar2275@gmail.com>
2025-07-31 10:58:15 +0530

    Delete Assiment directory

--- Assiment/Array/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main (){
-   // write a program  to print factuer in given range eg 10 to 15 
-
-   int arry[5]
-      printf("Enter the number ");
-
-      if (i=0,i<5,i++)
-}
